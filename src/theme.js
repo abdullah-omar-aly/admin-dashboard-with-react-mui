95ed48af8dc215ba4f73408b182d5a3594f4d12f
abdullah-omar-aly <abdullah.omar.aly@gmail.com>
2023-01-11 18:42:41 +0200

    refactor theme colors

--- src/theme.js
+++ src/theme.js
@@ -7,18 +7,23 @@ const getPaletteColors = (mode) => {
     ...(mode === 'dark'
       ? {
         // palette values for dark mode
-        primary: { main: '#4CC6AD' },
-        secondary: { main: '#3e4396' },
-        primary_bg: { main:  "#fff" },
-        secondary_bg: { main: '#151929' },
+        primary: { main: '#009688' },
+        secondary: { main: '#512DA8' },
+        primary_bg: { main:  "#151929" },
+        secondary_bg: { main: '#1C2538' },
+        primary_text: { main: "fff"},
+        secondary_text: { main: "#898FA1"}
 
       }
       : {
         // palette values for ligth mode
-        primary: { main: '#4CC6AD' },
-        secondary: { main: '#3e4396' },
-        primary_bg: { main: '#FF2A9C' },
+        primary: { main: '##009688' },
+        secondary: { main: '#512DA8' },
+        primary_bg: { main: '#e0e2e2' },
         secondary_bg: { main: '#eee' },
+        primary_text: { main: "#000000"},
+        secondary_text: { main: "#99989A"}
+
       }),
   }
 }
