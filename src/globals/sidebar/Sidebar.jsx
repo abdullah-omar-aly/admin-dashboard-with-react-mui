7b87137df8dfa9769276cab84a3916a05105e8d7
abdullah-omar-aly <abdullah.omar.aly@gmail.com>
2023-01-12 18:41:48 +0200

    implement routing (in-progress)

--- src/globals/sidebar/Sidebar.jsx
+++ src/globals/sidebar/Sidebar.jsx
@@ -1,9 +1,34 @@
-import { Avatar, Box, Typography } from '@mui/material'
-import React from 'react'
+import { Avatar, Box, Menu, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import { useSidebar } from './index'
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import { Link } from 'react-router-dom'
+
+const Item = ({ title, to, icon, selected, setSelected }) => {
+  // const theme = useTheme();
+  // const colors = tokens(theme.palette.mode);
+  return (
+    // <MenuItem
+    //   active={selected === title}
+    //   style={{
+    //     // color: colors.grey[100],
+    //   }}
+    //   onClick={() => setSelected(title)}
+    //   icon={icon}
+    // >
+    <>
+      <Typography>{title}</Typography>
+      <Link to={to} />
+    </>
+    // </MenuItem>
+  );
+};
 
 export function Sidebar() {
   const { isCollapsed } = useSidebar()
+  const [selected, setSelected] = useState("Dashboard");
 
 
   return (
@@ -33,7 +58,22 @@ export function Sidebar() {
           <Typography variant='h6' color="primary_text.main">Evan U</Typography>
           <Typography sx={{ color: "primary.main", fontSize: '0.7rem' }} >VP Fancy Admin</Typography>
         </Box>
-
+        
+        
+        <Box sx={{display: "flex" , flexDirection: 'column' , alignItems: "center"}} >
+          <Box sx={{display: 'flex' , paddingY: '0.5rem' }}>
+            <HomeOutlinedIcon />
+            <Typography color="green">Dashboard</Typography>
+          </Box>
+          <Box sx={{display: 'flex' , paddingY: '0.5rem' }}>
+            <HomeOutlinedIcon />
+            <Typography color="green">Dashboard</Typography>
+          </Box>
+          <Box sx={{display: 'flex' , paddingY: '0.5rem' }}>
+            <HomeOutlinedIcon />
+            <Typography color="green">Dashboard</Typography>
+          </Box>
+        </Box>
       </Box>
 
 
