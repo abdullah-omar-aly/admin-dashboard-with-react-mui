9279cf6ead95d536330f089322439b8ff93b6227
abdullah-omar-aly <abdullah.omar.aly@gmail.com>
2023-01-12 18:43:15 +0200

    complete refactor dashboard-scene

--- src/scenes/dashboard/Dashboard.jsx
+++ src/scenes/dashboard/Dashboard.jsx
@@ -7,9 +7,9 @@ import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
 import TrafficIcon from "@mui/icons-material/Traffic";
 import Header from "../../components/Header";
-import ProgressCircle from "../../components/ProgressCircle";
 import StatBox from "../../components/StatBox";
 import LineChart from "../../components/LineChart";
+
 export function Dashboard() {
   return (
     <Box m="20px">
@@ -121,8 +121,9 @@ export function Dashboard() {
         <Box
           gridColumn="span 8"
           gridRow="span 2"
-        //   backgroundColor={colors.primary[400]}
-        >
+          bgcolor="primary_bg.main"
+          borderRadius="0.5rem"
+         >
           <Box
             mt="25px"
             p="0 30px"
@@ -161,7 +162,6 @@ export function Dashboard() {
         <Box
           gridColumn="span 4"
           gridRow="span 2"
-          //   backgroundColor={colors.primary[400]}
           backgroundColor="primary_bg.main"
           overflow="auto"
           borderRadius="0.5rem"
@@ -171,7 +171,6 @@ export function Dashboard() {
             justifyContent="space-between"
             alignItems="center"
             borderBottom={`4px solid secondary_text.main`}
-            // colors={colors.grey[100]}
             p="15px"
           >
             <Typography color='secondary_text.main' variant="h5" fontWeight="600">
@@ -210,67 +209,7 @@ export function Dashboard() {
             </Box>
           ))}
         </Box>
-
-        {/* ROW 3 */}
-        {/* <Box
-          gridColumn="span 4"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
-          p="30px"
-        >
-          <Typography variant="h5" fontWeight="600">
-            Campaign
-          </Typography>
-          <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            mt="25px"
-          >
-            <ProgressCircle size="125" />
-            <Typography
-              variant="h5"
-              color={colors.greenAccent[500]}
-              sx={{ mt: "15px" }}
-            >
-              $48,352 revenue generated
-            </Typography>
-            <Typography>Includes extra misc expenditures and costs</Typography>
-          </Box>
-        </Box>
-        <Box
-          gridColumn="span 4"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
-        >
-          <Typography
-            variant="h5"
-            fontWeight="600"
-            sx={{ padding: "30px 30px 0 30px" }}
-          >
-            Sales Quantity
-          </Typography>
-          <Box height="250px" mt="-20px">
-            <BarChart isDashboard={true} />
-          </Box>
-        </Box>
-        <Box
-          gridColumn="span 4"
-          gridRow="span 2"
-          backgroundColor={colors.primary[400]}
-          padding="30px"
-        >
-          <Typography
-            variant="h5"
-            fontWeight="600"
-            sx={{ marginBottom: "15px" }}
-          >
-            Geography Based Traffic
-          </Typography>
-          <Box height="200px">
-            <GeographyChart isDashboard={true} />
-          </Box>
-        </Box> */}
+  
       </Box>
 
     </Box>
