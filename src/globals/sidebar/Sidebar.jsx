21f8855059e03d1e6c9006d19509773f8ca83742
abdullah-omar-aly <abdullah.omar.aly@gmail.com>
2023-01-13 17:36:56 +0200

    routing with react-router-dom completed

--- src/globals/sidebar/Sidebar.jsx
+++ src/globals/sidebar/Sidebar.jsx
@@ -1,35 +1,39 @@
-import { Avatar, Box, Menu, Typography } from '@mui/material'
-import React, { useState } from 'react'
+import { Avatar, Box, Typography, useTheme } from '@mui/material'
+import React from 'react'
 import { useSidebar } from './index'
+import { NavLink } from 'react-router-dom'
 import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
 import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
 import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
-import { Link } from 'react-router-dom'
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
 
-const Item = ({ title, to, icon, selected, setSelected }) => {
-  // const theme = useTheme();
-  // const colors = tokens(theme.palette.mode);
+const SidebarItem = ({ title, to, icon }) => {
+  const { palette } = useTheme();
+
   return (
-    // <MenuItem
-    //   active={selected === title}
-    //   style={{
-    //     // color: colors.grey[100],
-    //   }}
-    //   onClick={() => setSelected(title)}
-    //   icon={icon}
-    // >
-    <>
-      <Typography>{title}</Typography>
-      <Link to={to} />
-    </>
-    // </MenuItem>
+
+    <NavLink style={({ isActive }) => {
+      return {
+        display: "flex",
+        alignItems: "center",
+        padding: "0.8rem",
+        textDecoration: "none",
+        borderLeft: "2px solid",
+        borderColor: isActive ? palette.primary.main : "transparent",
+        background: isActive ? palette.secondary_bg.main : "transparent",
+
+        color: palette.primary_text.main
+      }
+    }} to={to}>
+
+      <Box component="span" sx={{ pr: "0.5rem" }}>{icon}</Box>
+      <Typography fontSize="0.9rem">{title}</Typography>
+    </NavLink>
   );
 };
 
 export function Sidebar() {
   const { isCollapsed } = useSidebar()
-  const [selected, setSelected] = useState("Dashboard");
-
 
   return (
     <Box
@@ -58,21 +62,13 @@ export function Sidebar() {
           <Typography variant='h6' color="primary_text.main">Evan U</Typography>
           <Typography sx={{ color: "primary.main", fontSize: '0.7rem' }} >VP Fancy Admin</Typography>
         </Box>
-        
-        
-        <Box sx={{display: "flex" , flexDirection: 'column' , alignItems: "center"}} >
-          <Box sx={{display: 'flex' , paddingY: '0.5rem' }}>
-            <HomeOutlinedIcon />
-            <Typography color="green">Dashboard</Typography>
-          </Box>
-          <Box sx={{display: 'flex' , paddingY: '0.5rem' }}>
-            <HomeOutlinedIcon />
-            <Typography color="green">Dashboard</Typography>
-          </Box>
-          <Box sx={{display: 'flex' , paddingY: '0.5rem' }}>
-            <HomeOutlinedIcon />
-            <Typography color="green">Dashboard</Typography>
-          </Box>
+
+
+        <Box sx={{ display: "flex", flexDirection: 'column' }} >
+          <SidebarItem title="Dashboard" to="/" icon={<HomeOutlinedIcon />} />
+          <SidebarItem title="Team" to="/team" icon={<PeopleOutlinedIcon />} />
+          <SidebarItem title="Calendar" to="/calendar" icon={<CalendarTodayOutlinedIcon />} />
+          <SidebarItem title="FAQ" to="/faq" icon={<HelpOutlineOutlinedIcon />} />
         </Box>
       </Box>
 
