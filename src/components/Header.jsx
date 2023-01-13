6b112b43b8990d2312c2ca3b23b056df7cca945c
abdullah-omar-aly <abdullah.omar.aly@gmail.com>
2023-01-13 20:45:17 +0200

    delete non important packages

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,7 @@
-import { Typography, Box, useTheme } from "@mui/material";
+import { Typography, Box } from "@mui/material";
 
 function Header({ title, subtitle }) {
-//   const theme = useTheme();
-//   const colors = tokens(theme.palette.mode);
+
   return (
     <Box mb="30px">
       <Typography
